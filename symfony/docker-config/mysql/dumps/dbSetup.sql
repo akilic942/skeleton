afbb99eefd7784b23c190d26c930e45bcaebf92a
Aziz Kilic <kaneda942@gmail.com>
2019-11-30 08:39:29 +0100

    Fix dbSetup

--- symfony/docker-config/mysql/dumps/dbSetup.sql
+++ symfony/docker-config/mysql/dumps/dbSetup.sql
@@ -2,6 +2,6 @@ CREATE USER 'db_user'@'%' IDENTIFIED BY 'db_user';
 
 CREATE DATABASE skeleton_db_local CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON skeleton_db_local.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON skeleton_db_local.* TO 'db_user'@'%';
 
 FLUSH PRIVILEGES;
